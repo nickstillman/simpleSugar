1e71efe4049216f29aca9ef83b425efe6986b2ac
Nick Stillman <nick.edward.stillman@gmail.com>
2021-03-04 18:14:57 -0500

    demo version works, ish

--- client/App.jsx
+++ client/App.jsx
@@ -64,6 +64,16 @@ class App extends Component {
     // convert date to log date
     
     this.getEntries(date);
+    
+    // update clock
+    
+    setInterval(() => {
+      const d = new Date()
+      // const date = d.toLocaleDateString().replace(/\//g, '-');
+      const time = d.toLocaleTimeString([], {hour: 'numeric', minute:'2-digit'});
+      console.log('new time:', time)
+      this.setState({displayTime: time})
+    }, 30000)//3600000)
   }
   
   getEntries(date) {
@@ -84,7 +94,6 @@ class App extends Component {
       });
     })
     .then(() => {
-      console.log('scroll inside:', this.state.scroll)
       if (this.state.scroll) this.scrollScreen();
     })
     .catch(err => console.error('App.componentDidMount: get entries: ERROR: ', err));
@@ -115,11 +124,79 @@ class App extends Component {
   
   processInput(val) {
     console.log('PROCESS THIS val:', val);
-    const entry = {"600": {"shot": 2, "bg": 67, "bgLabel": "pre", "time": 600, "notes": "OMG I CANT BELIEVE IT'S NOT"}};
-    const date = '3-4-2021';
+    val = val.slice(0, val.length - 1);
+    // const entry = {"600": {"shot": 2, "bg": 67, "bgLabel": "pre", "time": 600, "notes": "OMG I CANT BELIEVE IT'S NOT"}};
+    // const date = '3-4-2021';
+    
+    // parse input
+    const valArr = val.split(' ');
+    if (valArr[0] === 'back') {
+      this.setState({displayDate: 'log3-3-2021', displayTime: time, currentDate: '3-3-2021', onToday: false, scroll: true})
+      this.getEntries('3-3-2021');
+      return;
+      
+    } else if (val[0] === 'forward') {
+      
+    } else if (val[0] === 'now') {
+      const d = new Date()
+      const date = d.toLocaleDateString().replace(/\//g, '-');
+      const time = d.toLocaleTimeString([], {hour: 'numeric', minute:'2-digit'});
+      
+      this.setState({displayDate: date, displayTime: time, currentDate: date, onToday: true, scroll: true})
+      this.getEntries('3-4-2021');
+      return;
+    }
+    
+    let i = 0;
+    let time;
+    // let date;
+    let bg;
+    let shot;
+    let bgLabel = '';
+    let notes = '';
+    console.log(valArr)
+    while (i < valArr.length) {
+      let el = valArr[i];
+      if (typeof parseInt(el) === 'number') {
+        el = parseInt(el);
+        if (el < 20) {
+          shot = el;
+        } else if (el < 300) {
+          bg = el;
+        } else if (el < 1441) {
+          time = el;
+        }
+      } else if (el === 'wal') {
+        bgLabel = el;
+      } else {
+        notes = valArr.slice(i).join(' ');
+        break;
+      }
+      i++;
+    }    
+    
+    const d = new Date()
+    const date = d.toLocaleDateString().replace(/\//g, '-');
+    // const timeNow = d.toLocaleTimeString([], {hour: 'numeric', minute:'2-digit'});
+    if (!time) {
+      const h = d.getHours();
+      const t = d.getMinutes();
+      console.log('t:',  h, t)
+      const mins = ((h - 4) * 60) + t;
+      console.log('mins:', mins)
+      const minsRound = Math.round(mins/10) * 10;
+      console.log('minsRound:', minsRound)
+      time = minsRound;
+    }
+    const entry = {};
+    entry[time] = {shot, bg, bgLabel, time, notes};
+    
+    console.log('entry:', entry)
+    
+    
+    // add to database
     this.setState({scroll: false});
     this.insertEntries(entry, date);
-    // this.getEntries(date);
   }
   
   
